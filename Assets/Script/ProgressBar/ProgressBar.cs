770fa698e2222c207f8191f27b02468f51f56e11
TA <terrence.abramson@gmail.com>
2023-11-19 03:03:49 -0500

    Flesh out table interaction logic

--- Assets/Script/ProgressBar/ProgressBar.cs
+++ Assets/Script/ProgressBar/ProgressBar.cs
@@ -1,24 +1,57 @@
 using System;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
+    public GameObject canvas;
+
     public Slider bar;
     public float progressSpeed;
 
+    private const float INITIAL_PROGRESS_SPEED = 0.01f;
+
+    public bool isActive()
+    {
+        if(canvas.activeInHierarchy == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void doProgress()
+    {
+        canvas.SetActive(true);
+        bar.enabled = true;
+        bar.value = 1;
+    }
+
+    public void setProgressSpeed(float newSpeed)
+    {
+        progressSpeed = newSpeed;
+    }
+   
     // Start is called before the first frame update
     void Start()
     {
         bar.enabled = false;
-        bar.value = 1;
+        progressSpeed = INITIAL_PROGRESS_SPEED;
     }
 
     // Update is called once per frame
     void Update()
     {
-        bar.enabled = true;
         bar.value -= progressSpeed;
+        if(bar.value <= 0)
+        {
+            canvas.SetActive(false);
+            bar.enabled = false;
+        }
     }
 }
--- Assets/Script/Table_Interaction.cs
+++ Assets/Script/Table_Interaction.cs
@@ -1,26 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using TMPro;
 
 public class Table_Interaction : MonoBehaviour
 {
+    public GameObject table;
+
     public GameObject BuyUI;
     public GameObject FixUI;
     public GameObject UpgradeUI;
 
-    public bool bought;
-    public bool broken;
-    public bool completed;
+    public TextMeshProUGUI displayText;
+
+    public GameObject UpgradeBox1;
+    public GameObject UpgradeBox2;
+    public GameObject UpgradeBox3;
+
+    public ProgressBar progressBar;
+
+    public uint breakChance;
 
     private const uint MAX_UPGRADES = 3;
+    private const uint INITIAL_BREAKCHANCE = 300;
+
+    private bool bought;
+    private bool broken;
+    private bool completed;
+    private bool fixing;
     private uint numUpgrades;
 
     public void Trigger()
     {
-        Debug.Log("Reached here");
 
-        if(completed)
+        if(completed && !broken)
         {
             // Completed so do nothing
             return;
@@ -57,27 +72,89 @@ public class Table_Interaction : MonoBehaviour
         }
     }
 
-    void doUpgrade()
+    public void doBuy()
     {
-        if (!completed)
+        // Set the bought flag and close the Buy Menu
+        bought = true;
+        BuyUI.SetActive(false);
+    }
+
+    public void doUpgrade()
+    {
+        // Add a level to the upgrade
+        if (!completed && !progressBar.isActive())
         {
             numUpgrades++;
+            switch(numUpgrades)
+            {
+                case 3:
+                    // Turn on the third box
+                    UpgradeBox3.SetActive(true);
+                    displayText.SetText("Level 3");
+                    break;
+                case 2:
+                    // Turn on the second box
+                    UpgradeBox2.SetActive(true);
+                    displayText.SetText("Level 2");
+                    break;
+                case 1:
+                    // Turn on the first box
+                    UpgradeBox1.SetActive(true);
+                    displayText.SetText("Level 1");
+                    break;
+                case 0:
+                default:
+                    //do nothing
+                    break;
+            }
             if (numUpgrades == MAX_UPGRADES)
             {
+                // If completed, set the completed flag and close the Upgrade Menu
                 completed = true;
+                UpgradeUI.SetActive(false);
             }
+            progressBar.doProgress();
         }
+    }
+
+    public void doBreak()
+    {
+        // Set the broken flag, close the Upgrade Menu, change the sprite to broken
+        broken = true;
+        UpgradeUI.SetActive(false);
+    }
+
+    public void doFix()
+    {
+        // Start the progress bar and close the Fix Menu
+        fixing = true;
+        progressBar.doProgress();
+        FixUI.SetActive(false);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        breakChance = INITIAL_BREAKCHANCE;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Break every once in a while
+        int temp;
+        if (bought && !broken)
+        {
+            temp = UnityEngine.Random.Range(0, 1000000);
+            if (temp < breakChance)
+            {
+                doBreak();
+            }
+        }
+        if((fixing == true) && (!progressBar.isActive()))
+        {
+            fixing = false;
+            broken = false;
+        }
     }
 }
