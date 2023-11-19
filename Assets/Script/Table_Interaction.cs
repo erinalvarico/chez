7855d2acd62433a091a8b0f7ca59a70f8dcc4212
TA <terrence.abramson@gmail.com>
2023-11-18 23:40:12 -0500

    Add completion flag and logic to clear menus

--- Assets/Script/Table_Interaction.cs
+++ Assets/Script/Table_Interaction.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,6 +18,8 @@ public class Table_Interaction : MonoBehaviour
 
     public void Trigger()
     {
+        Debug.Log("Reached here");
+
         if(completed)
         {
             // Completed so do nothing
